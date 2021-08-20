6639494c61a89ce071bf25a6f0e7a960db389234
seoguri <yijk7928@naver.com>
2021-08-20 15:28:25 +0900

    init

--- src/main/java/com/example/myhome/controller/HomeController.java
+++ src/main/java/com/example/myhome/controller/HomeController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class HomeController {
 
     @GetMapping
-    public String index(){
+    public String index() {
         return "index";
     }
 
